d11c560f9c334d7d37a5d8b8f63d30118ba39f17
bbaa <bbaa@bbaa.me>
2026-09-13 23:06:06 +0800

    fix build on v7.2

--- include/drm/drm_atomic.h
+++ include/drm/drm_atomic.h
@@ -3,9 +3,15 @@
 #ifndef __BACKPORT_DRM_ATOMIC_H__
 #define __BACKPORT_DRM_ATOMIC_H__
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(7, 2, 0)
 static inline int __idb_shim_drm_atomic_commit(struct drm_atomic_state *state) {
 	return drm_atomic_commit(state);
 }
+#else
+static inline int __idb_shim_drm_atomic_commit(struct drm_atomic_commit *commit) {
+	return drm_atomic_commit(commit);
+}
+#endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(7, 2, 0)
 #define drm_atomic_commit                 drm_atomic_state
